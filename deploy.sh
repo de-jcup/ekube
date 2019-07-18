0f55887bc8fdd3a3ed4456135ed3c47cf0d11894
de-jcup <albert.tregnaghi@gmail.com>
2019-07-18 18:20:39 +0200

    Added final change to deploy script #49

--- deploy.sh
+++ deploy.sh
@@ -16,4 +16,4 @@ if [ -z "$BINTRAY_API_KEY" ]; then
 	echo "bintray api key not set"
 	show_help_and_exit;
 fi
-./pushToBintray.sh ${BINTRAY_USER} ${BINTRAY_API_KEY} de-jcup ekube update-site 0.5.0 ./de.jcup.ekube.updatesite
+./pushToBintray.sh ${BINTRAY_USER} ${BINTRAY_API_KEY} de-jcup ekube update-site 0.5.0 ./ekube-updatesite
